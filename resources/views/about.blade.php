3556ad72c023adbc0edcca42f62b90d366d0fe03
LorenzoOgnibene <lorenzo.ognibene@gmail.com>
2023-02-07 15:34:56 +0100

    add second page with nav bar

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,15 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome Laraver</title>
-</head>
-<body>
-    <header></header>
-    <main>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga nesciunt ut asperiores sint qui eaque pariatur iusto, natus esse aspernatur! Voluptates deserunt consectetur aperiam assumenda, minus excepturi alias veniam quam!</p>
-    </main>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        {{-- bootstrap --}}
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <title>Welcome Laraver</title>
+    </head>
+    <body>
+        <header>
+            <nav class="navbar navbar-expand-lg" style="background-color: #772ce8">
+                <div class="container-fluid w-auto">
+                  <a class="navbar-brand" href="#"></a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                      <li class="nav-item">
+                        <a class="nav-link active text-white fw-bold" aria-current="page" href="/">Home</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link text-white fw-bold" href="/about">About</a>
+                      </li>
+                      <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle text-white fw-bold" href="/categorie" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                          Categorie
+                        </a>
+                        <ul class="dropdown-menu">
+                          <li><a class="dropdown-item" href="#">Cerca per baffi</a></li>
+                          <li><a class="dropdown-item" href="#">Motosega</a></li>
+                          <li><hr class="dropdown-divider"></li>
+                          <li><a class="dropdown-item" href="#">Paperella</a></li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </nav>
+        </header>
+        <main>
+            <p class="text-danger">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga nesciunt ut asperiores sint qui eaque pariatur iusto, natus esse aspernatur! Voluptates deserunt consectetur aperiam assumenda, minus excepturi alias veniam quam!</p>
+        </main>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    </body>
 </html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,12 +4,44 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
         <title>Welcome Laravel</title>
     </head>
     <body>
-        <header></header>
+        <header>
+            <nav class="navbar navbar-expand-lg" style="background-color: #18181b">
+                <div class="container-fluid w-auto">
+                  <a class="navbar-brand" href="#"></a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                      <li class="nav-item">
+                        <a class="nav-link active text-white fw-bold" aria-current="page" href="/">Home</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link text-white fw-bold" href="/about">About</a>
+                      </li>
+                      <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle text-white fw-bold" href="/categorie" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                          Categorie
+                        </a>
+                        <ul class="dropdown-menu">
+                          <li><a class="dropdown-item" href="#">Cerca per baffi</a></li>
+                          <li><a class="dropdown-item" href="#">Motosega</a></li>
+                          <li><hr class="dropdown-divider"></li>
+                          <li><a class="dropdown-item" href="#">Paperella</a></li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </nav>
+        </header>
         <main>
             <h1>Hello world my first laravel page</h1>
         </main>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     </body>
 </html>
