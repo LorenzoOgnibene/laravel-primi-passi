179cb9322655cfc3b38e2bf5d6c36a612198a116
LorenzoOgnibene <lorenzo.ognibene@gmail.com>
2023-02-07 15:14:17 +0100

    My first laravel page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome Laraver</title>
+</head>
+<body>
+    <header></header>
+    <main>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga nesciunt ut asperiores sint qui eaque pariatur iusto, natus esse aspernatur! Voluptates deserunt consectetur aperiam assumenda, minus excepturi alias veniam quam!</p>
+    </main>
+</body>
+</html>
